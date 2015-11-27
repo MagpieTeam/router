03764f5e53933c2a8860b519d844365830c31e61
Troels Brødsgaard <tr@arbr.dk>
2015-11-27 09:47:48 +0100

    add decimal to_string, rework from_string

--- lib/router/decimal.ex
+++ lib/router/decimal.ex
@@ -1,14 +1,32 @@
 defmodule Router.Decimal do
 
   def from_string(value) when is_binary(value) do
-    String.split(value, ".")
-    case Regex.named_captures(~r/^(?<integer>\d*)\.?(?<rest>\d*)$/, value) do
-      %{"integer" => integer, "rest" => rest} -> 
-        scale = String.length(rest)
-        {String.to_integer(integer <> rest), scale}
-      %{"integer" => integer, "rest" => ""} ->
+    case String.split(value, ".") do
+      [integer, remainder] ->
+        scale = byte_size(remainder)
+        {String.to_integer(integer <> remainder), scale}
+      [integer] ->
         {String.to_integer(integer), 0}
     end
   end
-  
+
+  def to_string({unscaled, scale}) when unscaled < 0 do
+    abs = Router.Decimal.to_string({abs(unscaled), scale})
+    "-" <> abs
+  end
+
+  def to_string({unscaled, scale}) do
+    unscaled_string = Integer.to_string(unscaled)
+    digits = byte_size(unscaled_string)
+
+    case digits - scale do
+      x when is_integer(x) and x > 1 ->
+        << integer :: binary - size(x), remainder :: binary >> = unscaled_string
+        integer <> "." <> remainder
+      0 -> "0." <> unscaled_string
+      x when is_integer(x) and x < 1 ->
+        "0." <> String.duplicate("0", abs(x)) <> unscaled_string
+    end
+  end
+
 end
