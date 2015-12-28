52ace2bebda941fbf506a9ec7e326dfa9b0d019c
Troels Brødsgaard <tr@arbr.dk>
2015-12-28 21:46:21 +0100

    fake load balancing on channels

--- web/channels/logger_channel.ex
+++ web/channels/logger_channel.ex
@@ -2,11 +2,15 @@ defmodule Router.LoggerChannel do
   use Router.Web, :channel
 
   def join("loggers:" <> logger_id, _message, socket) do
-    :ok = Router.Presence.register(logger_id, self())
-    sensors = Magpie.DataAccess.Sensor.get(logger_id)
-    Enum.each(sensors, fn (s) -> Router.Aggregator.start_link(to_string(s[:id])) end)
-
-    {:ok, socket}
+    case Router.LoadBalancer.permit?() do
+      true ->
+        :ok = Router.Presence.register(logger_id, self())
+        sensors = Magpie.DataAccess.Sensor.get(logger_id)
+        Enum.each(sensors, fn (s) -> Router.Aggregator.start_link(to_string(s[:id])) end)
+        {:ok, socket}
+      false ->
+        {:error, "request denied due to overload protection"}
+    end
   end
   
   def handle_in("new_log", msg, socket) do
