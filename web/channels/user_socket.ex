b0f5f358f75324a3ca23791aad934c8f11249b42
Troels Brødsgaard <tr@arbr.dk>
2016-01-04 21:44:57 +0100

    disable origin checks for sensor channels

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Router.UserSocket do
   channel "sensors:*", Router.SensorChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
