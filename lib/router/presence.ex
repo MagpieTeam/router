34c10e7ecfc352c4b048612fc820f56a8ae6e91f
Troels Brødsgaard <tr@arbr.dk>
2016-01-05 12:37:49 +0100

    use Logger instead of IO

--- lib/router/presence.ex
+++ lib/router/presence.ex
@@ -37,7 +37,7 @@ defmodule Router.Presence do
     port = to_port(port_conf)
     endpoint_ip = "#{ip}:#{port}"
     :ets.insert(@nodes, {node, endpoint_ip})
-    
+
     {:ok, %{node: node, endpoint_ip: endpoint_ip}}
   end
 
@@ -45,7 +45,7 @@ defmodule Router.Presence do
   defp to_port({:system, env_var}), do: to_port(System.get_env(env_var))
 
   def handle_call({:register, logger_id, pid} = msg, _from, %{node: node} = state) do
-    IO.puts("Got loggerup #{inspect msg}")
+    Logger.debug("Got loggerup #{inspect msg}")
     Process.monitor(pid)
     true = :ets.insert(@loggers, {logger_id, pid, node, :online})
     # broadcast on gossip and local
@@ -57,7 +57,7 @@ defmodule Router.Presence do
 
   def handle_info({:DOWN, ref, _type, pid, {_info, reason}} = msg, %{node: node} = state) do
     # logger down, remove
-    IO.puts("Got :DOWN #{inspect msg}")
+    Logger.debug("Got :DOWN #{inspect msg}")
     # TODO: If ets returns nil, this is gonna crash. Please fix
     case :ets.match(@loggers, {:"$1", pid, node, :_}) |> List.first |> List.first do
       nil -> 
@@ -74,7 +74,7 @@ defmodule Router.Presence do
   end
 
   def handle_info(%Broadcast{event: "logger_up", payload: %{id: logger_id, node: node}}, state) do
-    IO.puts("Got remote logger_up: #{logger_id} on #{node}")
+    Logger.debug("Got remote logger_up: #{logger_id} on #{node}")
 
     true = :ets.insert(@loggers, {logger_id, nil, node, :online})
     broadcast = %Broadcast{event: "logger_up", topic: "loggers:status", payload: %{id: logger_id, node: node}}
@@ -83,7 +83,7 @@ defmodule Router.Presence do
   end
 
   def handle_info(%Broadcast{event: "logger_down", payload: %{id: logger_id, node: node}}, state) do
-    IO.puts("Got remote logger_down: #{logger_id} on #{node}")
+    Logger.debug("Got remote logger_down: #{logger_id} on #{node}")
 
     :ets.delete(@loggers, logger_id)
     broadcast = %Broadcast{event: "logger_down", topic: "loggers:status", payload: %{id: logger_id, node: node}}
@@ -92,7 +92,7 @@ defmodule Router.Presence do
   end
 
   def handle_info({:nodeup, remote_node} = msg, %{node: node, endpoint_ip: endpoint_ip} = state) do
-    IO.puts("Got remote node up: #{remote_node}")
+    Logger.debug("Got remote node up: #{remote_node}")
     # When two nodes connect, both receive this message
     # Each node sends a list to the remote node containing
     # a list of loggers on the local node
@@ -102,7 +102,7 @@ defmodule Router.Presence do
   end
 
   def handle_info({:nodedown, node} = msg, state) do
-    IO.puts("Got remote node down: #{node}")
+    Logger.debug("Got remote node down: #{node}")
 
     logger_ids = :ets.match(@loggers, {:"$1", :_, node, :online})
     loggers = Enum.map(logger_ids, fn([ l | _]) -> {l, nil, node, :unknown} end)
@@ -115,7 +115,7 @@ defmodule Router.Presence do
   end
 
   def handle_cast({:loggers, loggers, remote_node, endpoint_ip}, %{node: node} = state) do
-    IO.puts("Got list of loggers from remote node: #{remote_node}: #{inspect loggers}")
+    Logger.debug("Got list of loggers from remote node: #{remote_node}: #{inspect loggers}")
 
     # delete all for this node, then insert the new ones
     # the genserver serialises all updates, thus, if a logger comes online on another node
