32eb653d3e11fac1ca68dcd507cbd48d419bbe51
Troels Brødsgaard <tr@arbr.dk>
2016-01-06 14:16:57 +0100

    pull info on presence from logger_channel

--- web/channels/logger_channel.ex
+++ web/channels/logger_channel.ex
@@ -1,6 +1,21 @@
 defmodule Router.LoggerChannel do
   use Router.Web, :channel
 
+  def join("loggers:status", _message, socket) do
+    send(self(), :status_join)
+
+    {:ok, socket}
+  end
+
+  def handle_info(:status_join, socket) do
+    loggers = Magpie.DataAccess.Logger.get()
+    status = Enum.map(loggers, fn(l) ->
+      Magpie.Presence.get_status(l[:id])
+    end)
+
+    push(socket, "new_status", %{status: status})
+  end
+
   def join("loggers:" <> logger_id, _message, socket) do
     sensors = Magpie.DataAccess.Sensor.get(logger_id)
     sensors_no = Enum.count(sensors)
